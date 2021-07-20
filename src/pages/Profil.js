b55f21ba1f4e7938469e7b91befd2bf810654454
Clément <clement.bednarek@gmail.com>
2021-07-20 17:58:48 +0200

    envois requete post login

--- src/pages/Profil.js
+++ src/pages/Profil.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div className="profil-page">
       <div className="log-container">
-        <Log />
+        <Log signin={false} signup={true}/> {/*sur sign up à l'ouverture de la page profil*/}
         <div className="img-container">
           <img src="./img/log.svg" alt=""/>
         </div>
