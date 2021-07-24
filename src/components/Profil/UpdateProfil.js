95b932f6c9374d636fd2aa98c0f2af13de1db6e3
Clément <clement.bednarek@gmail.com>
2021-07-24 13:59:00 +0200

    page profil fonctionnelle (sans follow handler)

--- src/components/Profil/UpdateProfil.js
+++ src/components/Profil/UpdateProfil.js
@@ -3,13 +3,17 @@ import LeftNav from '../LeftNav';
 import { useDispatch, useSelector } from 'react-redux';
 import UploadImg from './UploadImg';
 import { updateBio } from "../../actions/user.actions";
+import { dateParser } from '../Utils';
 
 
 const UpdateProfil = () => {
     const[bio, setBio] = useState('');
     const [updateForm, setUpdateForm] = useState(false);
-    const userData = useSelector((state) => state.userReducer)
+    const userData = useSelector((state) => state.userReducer);
+    const usersData = useSelector((state) => state.usersReducer);
     const dispatch = useDispatch();
+    const [followingPopup, setFollowingPopup] = useState(false);
+    const [followersPopup, setFollowersPopup] = useState(false);
 
     const handleUpdate = () => {
         dispatch(updateBio(userData._id, bio));
@@ -45,10 +49,69 @@ const UpdateProfil = () => {
                             <button onClick={handleUpdate}>Valider modification</button>
                             </>
                         )}
-
                     </div>
+                    <h4>Inscrit depuis le : {dateParser(userData.createdAt)}</h4>
+                    <h5 onClick={() => setFollowingPopup(true)}>
+                        Abonnement : {userData.following ? userData.following.length : ""}
+                    </h5> {/*s'il y a des abonnées (au chargement de la page le nombre n'ai pas dispo donc on rend la fonction asyncronne)*/}
+                    <h5 onClick={() => setFollowersPopup(true)}>
+                        Abonnés : {userData.followers ? userData.followers.length :""}
+                    </h5>
                 </div>
             </div>
+            {followingPopup && (
+                <div className="popup-profil-container">
+                    <div className="modal">
+                        <h3>Abonnements</h3>
+                        <span className="cross" onClick={() => setFollowingPopup(false)} >
+                            &#10005; {/*code html pour faire une crois */}
+                        </span>
+                        <ul>
+                            {usersData.map((user) => { //map de tout les utilisateurs
+                                for (let i = 0; i< userData.following.length; i++) { //prend tout les abonnements
+                                    if (user._id === userData.following[i]) { //retourn quans les id match
+                                        return (
+                                            <li key={user._id}>
+                                                <img src={`${process.env.REACT_APP_API_URL}${user.picture}`} alt="user-pic"/>
+                                                <h4>{user.pseudo}</h4>
+                                                <h1>FOLLOW HANDLER</h1>
+                                            </li>
+                                        )
+                                    }
+
+                                }
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            )}
+
+{followersPopup && (
+                <div className="popup-profil-container">
+                    <div className="modal">
+                        <h3>Abonnements</h3>
+                        <span className="cross" onClick={() => setFollowersPopup(false)} >
+                            &#10005; {/*code html pour faire une crois */}
+                        </span>
+                        <ul>
+                            {usersData.map((user) => { //map de tout les utilisateurs
+                                for (let i = 0; i< userData.followers.length; i++) { //prend tout les abonnements
+                                    if (user._id === userData.followers[i]) { //retourn quans les id match
+                                        return (
+                                            <li key={user._id}>
+                                                <img src={`${process.env.REACT_APP_API_URL}${user.picture}`} alt="user-pic"/>
+                                                <h4>{user.pseudo}</h4>
+                                                <h1>FOLLOW HANDLER</h1>
+                                            </li>
+                                        )
+                                    }
+
+                                }
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            )}
         </div>
     )
 };
--- src/index.js
+++ src/index.js
@@ -6,14 +6,18 @@ import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers'
+import { getUsers } from './actions/users.actions';
 
 // outils de développement
 import { composeWithDevTools } from 'redux-devtools-extension'; //les infos sont dans l'outil sur la barre du navigateur
 import logger from 'redux-logger'; //les infos sont dans la console
 
+
 const store = createStore(
   rootReducer, composeWithDevTools(applyMiddleware(thunk, logger))
 )
+
+store.dispatch(getUsers()); //récupère tout les utilisateurs dès le lancement de l'app
 
 ReactDOM.render(
   <Provider store={store}>
